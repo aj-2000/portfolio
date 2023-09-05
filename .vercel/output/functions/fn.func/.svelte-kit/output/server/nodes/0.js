2da8d7b980bfc8b41f7f314a90e55ae9185113dc
Ajay Sharma <hi@ajaysharma.dev>
2023-09-06 02:52:06 +0530

    remove .vercel folder

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.6d943c83.js","_app/immutable/chunks/scheduler.1a2feedf.js","_app/immutable/chunks/index.cec3ed25.js"];
-export const stylesheets = ["_app/immutable/assets/0.c3de7b9b.css"];
-export const fonts = [];
